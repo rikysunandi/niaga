3b3cb79bc16ae0eb1d223c0e80eba6dc66f92b96
rikysunandi <riky.sunandi@gmail.com>
2022-02-07 18:42:35 +0700

    Update index.php

--- services/pelanggan/index.php
+++ services/pelanggan/index.php
@@ -8,50 +8,92 @@ require_once '../../bo/config/database.php';
 // error_reporting(E_ALL);
 ini_set('memory_limit', '-1');
 
+$param = $_GET['param'];
 $unitupi = $_GET['unitupi'];
 $unitap = $_GET['unitap'];
 $unitup = $_GET['unitup'];
 $user = 'SYSTEM';
 
-$params = array(
-        array($user, SQLSRV_PARAM_IN),
-        array($unitupi, SQLSRV_PARAM_IN),
-        array($unitap, SQLSRV_PARAM_IN),
-        array($unitup, SQLSRV_PARAM_IN),
-    );
+if($param <> ''){
+	$params = array(
+	        array($user, SQLSRV_PARAM_IN),
+	        array($param, SQLSRV_PARAM_IN),
+	    );
 
-$sql = "EXEC sp_WS_Get_DIL_Unit @UserID = ?, @Unitupi = ?, @Unitap = ?, @Unitup = ? ";
-$stmt = sqlsrv_prepare($conn, $sql, $params);
+	$sql = "EXEC sp_WS_Get_DIL_By_Param @UserID = ?, @Param = ? ";
+	$stmt = sqlsrv_prepare($conn, $sql, $params);
 
-//sqlsrv_execute($stmt);
-if(sqlsrv_execute($stmt)){
-	$i=0;
-	while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-		$response['pelanggan'][$i]['unitupi'] = $row['UNITUPI']; 
-		$response['pelanggan'][$i]['unitap'] = $row['UNITAP']; 
-		$response['pelanggan'][$i]['unitup'] = $row['UNITUP']; 
-		$response['pelanggan'][$i]['idpel'] = $row['IDPEL']; 
-		$response['pelanggan'][$i]['nama'] = $row['NAMA']; 
-		$response['pelanggan'][$i]['tarif'] = $row['TARIF']; 
-		$response['pelanggan'][$i]['daya'] = $row['DAYA'];
-		$response['pelanggan'][$i]['gardu'] = $row['NAMA_GARDU'];
-		$response['pelanggan'][$i]['string_1'] = $row['NOMOR_JURUSAN_TIANG'];
-		$response['pelanggan'][$i]['kddk'] = $row['KDDK'];
-		$response['pelanggan'][$i]['nomor_meter_kwh'] = $row['NOMOR_METER_KWH'];
-		$response['pelanggan'][$i]['notelp'] = $row['NOTELP'];
-		$response['pelanggan'][$i]['latitude'] = $row['LATITUDE'];
-		$response['pelanggan'][$i]['longitude'] = $row['LONGITUDE'];
+	//sqlsrv_execute($stmt);
+	if(sqlsrv_execute($stmt)){
+		$i=0;
+		while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+			$response['pelanggan']['unitupi'] = $row['UNITUPI']; 
+			$response['pelanggan']['unitap'] = $row['UNITAP']; 
+			$response['pelanggan']['unitup'] = $row['UNITUP']; 
+			$response['pelanggan']['idpel'] = $row['IDPEL']; 
+			$response['pelanggan']['nama'] = $row['NAMA']; 
+			$response['pelanggan']['tarif'] = $row['TARIF']; 
+			$response['pelanggan']['daya'] = $row['DAYA'];
+			$response['pelanggan']['gardu'] = $row['NAMA_GARDU'];
+			$response['pelanggan']['string_1'] = $row['NOMOR_JURUSAN_TIANG'];
+			$response['pelanggan']['kddk'] = $row['KDDK'];
+			$response['pelanggan']['nomor_meter_kwh'] = $row['NOMOR_METER_KWH'];
+			$response['pelanggan']['notelp'] = $row['NOTELP'];
+			$response['pelanggan']['latitude'] = $row['LATITUDE'];
+			$response['pelanggan']['longitude'] = $row['LONGITUDE'];
 
-		$i++;
+			$i++;
+		}
+
+		$response['success'] = true;
+		sqlsrv_free_stmt($stmt);
+
+
+	}else{
+		$response['success'] = false;
+		$response['msg'] = 'Gagal melakukan Query ke Database';
 	}
-
-	$response['success'] = true;
-	sqlsrv_free_stmt($stmt);
-
-
 }else{
-	$response['success'] = false;
-	$response['msg'] = 'Gagal melakukan Query ke Database';
+	$params = array(
+	        array($user, SQLSRV_PARAM_IN),
+	        array($unitupi, SQLSRV_PARAM_IN),
+	        array($unitap, SQLSRV_PARAM_IN),
+	        array($unitup, SQLSRV_PARAM_IN),
+	    );
+
+	$sql = "EXEC sp_WS_Get_DIL_Unit @UserID = ?, @Unitupi = ?, @Unitap = ?, @Unitup = ? ";
+	$stmt = sqlsrv_prepare($conn, $sql, $params);
+
+	//sqlsrv_execute($stmt);
+	if(sqlsrv_execute($stmt)){
+		$i=0;
+		while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+			$response['pelanggan'][$i]['unitupi'] = $row['UNITUPI']; 
+			$response['pelanggan'][$i]['unitap'] = $row['UNITAP']; 
+			$response['pelanggan'][$i]['unitup'] = $row['UNITUP']; 
+			$response['pelanggan'][$i]['idpel'] = $row['IDPEL']; 
+			$response['pelanggan'][$i]['nama'] = $row['NAMA']; 
+			$response['pelanggan'][$i]['tarif'] = $row['TARIF']; 
+			$response['pelanggan'][$i]['daya'] = $row['DAYA'];
+			$response['pelanggan'][$i]['gardu'] = $row['NAMA_GARDU'];
+			$response['pelanggan'][$i]['string_1'] = $row['NOMOR_JURUSAN_TIANG'];
+			$response['pelanggan'][$i]['kddk'] = $row['KDDK'];
+			$response['pelanggan'][$i]['nomor_meter_kwh'] = $row['NOMOR_METER_KWH'];
+			$response['pelanggan'][$i]['notelp'] = $row['NOTELP'];
+			$response['pelanggan'][$i]['latitude'] = $row['LATITUDE'];
+			$response['pelanggan'][$i]['longitude'] = $row['LONGITUDE'];
+
+			$i++;
+		}
+
+		$response['success'] = true;
+		sqlsrv_free_stmt($stmt);
+
+
+	}else{
+		$response['success'] = false;
+		$response['msg'] = 'Gagal melakukan Query ke Database';
+	}
 }
 
 header('Content-Type: application/json');
