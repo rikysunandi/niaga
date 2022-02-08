6d3198a6c48ae600272958a01ed07f38483928ae
rikysunandi <riky.sunandi@gmail.com>
2022-02-08 17:00:11 +0700

    Update index.php

--- services/pemeriksaan_lpb/add/index.php
+++ services/pemeriksaan_lpb/add/index.php
@@ -111,10 +111,10 @@ $stmt = sqlsrv_prepare($conn, $sql, $params);
 if(sqlsrv_execute($stmt)){
     sqlsrv_next_result($stmt);
     $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-    if($row['IDPEL']==''){
-        sqlsrv_next_result($stmt);
-        $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-    }
+    // if($row['IDPEL']==''){
+    //     sqlsrv_next_result($stmt);
+    //     $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+    // }
 
     $response = $row;
 
