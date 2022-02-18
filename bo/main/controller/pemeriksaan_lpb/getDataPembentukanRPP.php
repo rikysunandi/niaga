ae3d4982e67250822f13254f739bbeb33f1a6fa0
rikysunandi <riky.sunandi@gmail.com>
2022-02-18 21:26:13 +0700

    Update getDataPembentukanRPP.php

--- bo/main/controller/pemeriksaan_lpb/getDataPembentukanRPP.php
+++ bo/main/controller/pemeriksaan_lpb/getDataPembentukanRPP.php
@@ -6,6 +6,7 @@ require_once '../../../config/database.php';
 $unitupi = $_GET['unitupi'];
 $unitap = $_GET['unitap'];
 $unitup = $_GET['unitup'];
+$petugas = $_GET['petugas'];
 $tgl_pemeriksaan_from = $_GET['tgl_pemeriksaan_from'];
 $tgl_pemeriksaan_to = $_GET['tgl_pemeriksaan_to'];
 $user = 'SYSTEM';
@@ -15,11 +16,12 @@ $params = array(
         array($unitupi, SQLSRV_PARAM_IN),
         array($unitap, SQLSRV_PARAM_IN),
         array($unitup, SQLSRV_PARAM_IN),
+        array($petugas, SQLSRV_PARAM_IN),
         array($tgl_pemeriksaan_from, SQLSRV_PARAM_IN),
         array($tgl_pemeriksaan_to, SQLSRV_PARAM_IN),
     );
 
-$sql = "EXEC sp_vw_Create_Pemeriksaan_LPB_Unit @UserID = ?, @Unitupi = ?, @Unitap = ?, @Unitup = ?, @Tgl_Pemeriksaan_From = ?, @Tgl_Pemeriksaan_To = ? ";
+$sql = "EXEC sp_vw_Create_Pemeriksaan_LPB_Unit @UserID = ?, @Unitupi = ?, @Unitap = ?, @Unitup = ?, @User_Input = ?, @Tgl_Pemeriksaan_From = ?, @Tgl_Pemeriksaan_To = ? ";
 $stmt = sqlsrv_prepare($conn, $sql, $params);
 
 
