971527f5b2a0009c86021862ecc1c1f91f49fcaf
rikysunandi <riky.sunandi@gmail.com>
2022-07-01 11:59:05 +0700

    blth

--- bo/main/template/mon-pemutusan.php
+++ bo/main/template/mon-pemutusan.php
@@ -101,7 +101,7 @@
                                                 <select id="sel_blth" class="selectpicker select-sm show-tick" data-size="5">
                                                     <option value="<?php echo date('Ym') ?>" selected="selected"><?php echo date('Ym') ?></option>
                                                     <option data-divider="true"></option>
-                                                    <option value='<?php echo date('Ym', strtotime("-2 month")) ?>'><?php echo date('Ym', strtotime("-1 month")) ?></option>
+                                                    <option value='<?php echo date('Ym', strtotime("-1 month")) ?>'><?php echo date('Ym', strtotime("-1 month")) ?></option>
                                                     <option value='<?php echo date('Ym', strtotime("-2 month")) ?>'><?php echo date('Ym', strtotime("-2 month")) ?></option>
                                                     <option value='<?php echo date('Ym', strtotime("-3 month")) ?>'><?php echo date('Ym', strtotime("-3 month")) ?></option>
                                                 </select>
--- bo/main/template/untitled.php
+++ bo/main/template/untitled.php
@@ -7,6 +7,6 @@
 
                                                     <option value="<?php echo date('Ym') ?>" selected="selected"><?php echo date('Ym') ?></option>
                                                     <option data-divider="true"></option>
-                                                    <option value='<?php echo date('Ym', strtotime("-".cal_days_in_month(CAL_GREGORIAN,date('m'),date('Y'))." day")) ?>'><?php echo date('Ym', strtotime("-".cal_days_in_month(CAL_GREGORIAN,date('m'),date('Y'))." day")) ?></option>
+                                                    <option value='<?php echo date('Ym', strtotime("-1 month")) ?>'><?php echo date('Ym', strtotime("-1 month")) ?></option>
                                                     <option value='<?php echo date('Ym', strtotime("-2 month")) ?>'><?php echo date('Ym', strtotime("-2 month")) ?></option>
                                                     <option value='<?php echo date('Ym', strtotime("-3 month")) ?>'><?php echo date('Ym', strtotime("-3 month")) ?></option>
