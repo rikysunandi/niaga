ee57bd927f735a2eb39a063c13c09258f40cf58a
rikysunandi <riky.sunandi@gmail.com>
2022-07-05 11:34:19 +0700

    Update index.php

--- services/pemeriksaan_lpb/add/index.php
+++ services/pemeriksaan_lpb/add/index.php
@@ -43,133 +43,242 @@ if(substr($idpel, 0, 11)=='99999999999'){
   //210429
 }
 
-$sql = 'SELECT * FROM m_pemeriksaan_lpb WHERE IDPEL = \''.$idpel.'\' AND FOTO_RUMAH IS NOT NULL ';
-//$params = array(1, substr($idpel,0,11));
+// $sql = 'SELECT * FROM m_pemeriksaan_lpb WHERE IDPEL = \''.$idpel.'\' AND FOTO_RUMAH IS NOT NULL ';
+// //$params = array(1, substr($idpel,0,11));
 
-$stmt = sqlsrv_query( $conn, $sql );
-if( $stmt === false ) {
-    $response['success'] = false;
-    $response['msg'] = 'Gagal query ke Database';
-}else{
-  $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-  if(1==0 && strlen($row['IDPEL'])>=11  && $row['USER_INPUT']!=$user_input){
-    $response['success'] = true;
-    $response['msg'] = 'Data Idpel '.$idpel.' tidak disimpan karena sudah pernah ditagging sebelumnya!';
-  }else{
+// $stmt = sqlsrv_query( $conn, $sql );
+// if( $stmt === false ) {
+//     $response['success'] = false;
+//     $response['msg'] = 'Gagal query ke Database';
+// }else{
+//   $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+//   if(1==0 && strlen($row['IDPEL'])>=11  && $row['USER_INPUT']!=$user_input){
+//     $response['success'] = true;
+//     $response['msg'] = 'Data Idpel '.$idpel.' tidak disimpan karena sudah pernah ditagging sebelumnya!';
+//   }else{
 
-    if(isset($_FILES['image'])){
-      $file_name = $idpel.'_'.$tgl_pemeriksaan.'.jpg';
-      $file_size =$_FILES['image']['size'];
-      $file_tmp =$_FILES['image']['tmp_name'];
-      $file_type=$_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+//     if(isset($_FILES['image'])){
+//       $file_name = $idpel.'_'.$tgl_pemeriksaan.'.jpg';
+//       $file_size =$_FILES['image']['size'];
+//       $file_tmp =$_FILES['image']['tmp_name'];
+//       $file_type=$_FILES['image']['type'];
+//       $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
 
-      $extensions= array("jpeg","jpg","png");
+//       $extensions= array("jpeg","jpg","png");
 
-      if(in_array($file_ext,$extensions)=== false){
-         $response['error'][]="extension not allowed, please choose a JPEG or PNG file.";
-      }
+//       if(in_array($file_ext,$extensions)=== false){
+//          $response['error'][]="extension not allowed, please choose a JPEG or PNG file.";
+//       }
 
-      if($file_size > 2097152){
-         $response['error'][]='File size must be excately 2 MB';
-      }
+//       if($file_size > 2097152){
+//          $response['error'][]='File size must be excately 2 MB';
+//       }
 
-      if(empty($response['error'])==true){
-        if(strlen($unitup)==5)
-          $file_path = "/media/nas/uploads/PRIANGAN/tagging/".$unitap."/".$unitup;
-        else
-          $file_path = "/media/nas/uploads/PRIANGAN/tagging/53XXX";
-        if (!file_exists($file_path)) {
-            mkdir($file_path, 0777, true);
-        }
-        if(move_uploaded_file($file_tmp, $file_path."/".$file_name))
-          $foto = $file_name;
-      }
+//       if(empty($response['error'])==true){
+//         if(strlen($unitup)==5)
+//           $file_path = "/media/nas/uploads/PRIANGAN/tagging/".$unitap."/".$unitup;
+//         else
+//           $file_path = "/media/nas/uploads/PRIANGAN/tagging/53XXX";
+//         if (!file_exists($file_path)) {
+//             mkdir($file_path, 0777, true);
+//         }
+//         if(move_uploaded_file($file_tmp, $file_path."/".$file_name))
+//           $foto = $file_name;
+//       }
+//     }
+
+//     if(isset($_FILES['image2'])){
+//       $file_name = $kk.'.jpg';
+//       $file_size =$_FILES['image2']['size'];
+//       $file_tmp =$_FILES['image2']['tmp_name'];
+//       $file_type=$_FILES['image2']['type'];
+//       $file_ext=strtolower(end(explode('.',$_FILES['image2']['name'])));
+
+//       $extensions= array("jpeg","jpg","png");
+
+//       if(in_array($file_ext,$extensions)=== false){
+//          $response['error'][]="extension not allowed, please choose a JPEG or PNG file.";
+//       }
+
+//       if($file_size > 2097152){
+//          $response['error'][]='File size must be excately 2 MB';
+//       }
+
+//       if(empty($response['error'])==true){
+//         if(strlen($unitup)==5)
+//           $file_path = "/media/nas/uploads/PRIANGAN/rumah/".$unitap."/".$unitup;
+//         else
+//           $file_path = "/media/nas/uploads/PRIANGAN/rumah/53XXX";
+//         if (!file_exists($file_path)) {
+//             mkdir($file_path, 0777, true);
+//         }
+//         if(move_uploaded_file($file_tmp, $file_path."/".$file_name))
+//           $foto_rumah = $file_name;
+//       }
+//     }
+
+//     sqlsrv_free_stmt($stmt);
+
+//     $params = array(
+//             array($tgl_pemeriksaan, SQLSRV_PARAM_IN),
+//             array($unitupi, SQLSRV_PARAM_IN),
+//             array($unitap, SQLSRV_PARAM_IN),
+//             array($unitup, SQLSRV_PARAM_IN),
+//             array($idpel, SQLSRV_PARAM_IN),
+//             array($nama, SQLSRV_PARAM_IN),
+//             array($tarif, SQLSRV_PARAM_IN),
+//             array($daya, SQLSRV_PARAM_IN),
+//             array($nik, SQLSRV_PARAM_IN),
+//             array($kk, SQLSRV_PARAM_IN),
+//             array($nohp, SQLSRV_PARAM_IN),
+//             array($email, SQLSRV_PARAM_IN),
+//             array($peruntukan, SQLSRV_PARAM_IN),
+//             array($sisa_kwh, SQLSRV_PARAM_IN),
+//             array($foto, SQLSRV_PARAM_IN),
+//             array($foto_rumah, SQLSRV_PARAM_IN),
+//             array($latitude, SQLSRV_PARAM_IN),
+//             array($longitude, SQLSRV_PARAM_IN),
+//             array($akurasi_koordinat, SQLSRV_PARAM_IN),
+//             array($user_input, SQLSRV_PARAM_IN),
+//             array($tgl_input, SQLSRV_PARAM_IN),
+//         );
+
+//     $sql = "EXEC SP_WS_PEMERIKSAAN_LPB_SIMPAN @TGL_PEMERIKSAAN = ?, @UNITUPI = ?, @UNITAP = ?, @UNITUP = ?, @IDPEL = ?, @NAMA = ?, @TARIF = ?, @DAYA = ?, @NIK = ?, @KK = ?, @NOHP = ?, @EMAIL = ?, @PERUNTUKAN = ?, @SISA_KWH = ?, @FOTO = ?, @FOTO_RUMAH = ?, @LATITUDE = ?, @LONGITUDE = ?, @AKURASI_KOORDINAT = ?, @USER_INPUT = ?, @TGL_INPUT = ? ";
+//     $stmt = sqlsrv_prepare($conn, $sql, $params);
+
+//     if(sqlsrv_execute($stmt)){
+//         //sqlsrv_next_result($stmt);
+//         $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+//         if($row['IDPEL']==''){
+//             sqlsrv_next_result($stmt);
+//             $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+//         }
+
+//         $response['pemeriksaan_lpb'] = $row;
+
+//         // sqlsrv_next_result($stmt);
+//         // $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+
+//         $response['msg'] = 'Data Idpel '.$idpel.' berhasil disimpan';
+//         $response['success'] = true;
+
+//     }else{
+//         $response['success'] = false;
+//         $response['msg'] = 'Data Idpel '.$idpel.' gagal disimpan';
+//     }
+
+//   }
+// }
+
+if(isset($_FILES['image'])){
+  $file_name = $idpel.'_'.$tgl_pemeriksaan.'.jpg';
+  $file_size =$_FILES['image']['size'];
+  $file_tmp =$_FILES['image']['tmp_name'];
+  $file_type=$_FILES['image']['type'];
+  $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+
+  $extensions= array("jpeg","jpg","png");
+
+  if(in_array($file_ext,$extensions)=== false){
+     $response['error'][]="extension not allowed, please choose a JPEG or PNG file.";
+  }
+
+  if($file_size > 2097152){
+     $response['error'][]='File size must be excately 2 MB';
+  }
+
+  if(empty($response['error'])==true){
+    if(strlen($unitup)==5)
+      $file_path = "/media/nas/uploads/PRIANGAN/tagging/".$unitap."/".$unitup;
+    else
+      $file_path = "/media/nas/uploads/PRIANGAN/tagging/53XXX";
+    if (!file_exists($file_path)) {
+        mkdir($file_path, 0777, true);
     }
-
-    if(isset($_FILES['image2'])){
-      $file_name = $kk.'.jpg';
-      $file_size =$_FILES['image2']['size'];
-      $file_tmp =$_FILES['image2']['tmp_name'];
-      $file_type=$_FILES['image2']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image2']['name'])));
-
-      $extensions= array("jpeg","jpg","png");
-
-      if(in_array($file_ext,$extensions)=== false){
-         $response['error'][]="extension not allowed, please choose a JPEG or PNG file.";
-      }
-
-      if($file_size > 2097152){
-         $response['error'][]='File size must be excately 2 MB';
-      }
-
-      if(empty($response['error'])==true){
-        if(strlen($unitup)==5)
-          $file_path = "/media/nas/uploads/PRIANGAN/rumah/".$unitap."/".$unitup;
-        else
-          $file_path = "/media/nas/uploads/PRIANGAN/rumah/53XXX";
-        if (!file_exists($file_path)) {
-            mkdir($file_path, 0777, true);
-        }
-        if(move_uploaded_file($file_tmp, $file_path."/".$file_name))
-          $foto_rumah = $file_name;
-      }
-    }
-
-    sqlsrv_free_stmt($stmt);
-
-    $params = array(
-            array($tgl_pemeriksaan, SQLSRV_PARAM_IN),
-            array($unitupi, SQLSRV_PARAM_IN),
-            array($unitap, SQLSRV_PARAM_IN),
-            array($unitup, SQLSRV_PARAM_IN),
-            array($idpel, SQLSRV_PARAM_IN),
-            array($nama, SQLSRV_PARAM_IN),
-            array($tarif, SQLSRV_PARAM_IN),
-            array($daya, SQLSRV_PARAM_IN),
-            array($nik, SQLSRV_PARAM_IN),
-            array($kk, SQLSRV_PARAM_IN),
-            array($nohp, SQLSRV_PARAM_IN),
-            array($email, SQLSRV_PARAM_IN),
-            array($peruntukan, SQLSRV_PARAM_IN),
-            array($sisa_kwh, SQLSRV_PARAM_IN),
-            array($foto, SQLSRV_PARAM_IN),
-            array($foto_rumah, SQLSRV_PARAM_IN),
-            array($latitude, SQLSRV_PARAM_IN),
-            array($longitude, SQLSRV_PARAM_IN),
-            array($akurasi_koordinat, SQLSRV_PARAM_IN),
-            array($user_input, SQLSRV_PARAM_IN),
-            array($tgl_input, SQLSRV_PARAM_IN),
-        );
-
-    $sql = "EXEC SP_WS_PEMERIKSAAN_LPB_SIMPAN @TGL_PEMERIKSAAN = ?, @UNITUPI = ?, @UNITAP = ?, @UNITUP = ?, @IDPEL = ?, @NAMA = ?, @TARIF = ?, @DAYA = ?, @NIK = ?, @KK = ?, @NOHP = ?, @EMAIL = ?, @PERUNTUKAN = ?, @SISA_KWH = ?, @FOTO = ?, @FOTO_RUMAH = ?, @LATITUDE = ?, @LONGITUDE = ?, @AKURASI_KOORDINAT = ?, @USER_INPUT = ?, @TGL_INPUT = ? ";
-    $stmt = sqlsrv_prepare($conn, $sql, $params);
-
-    if(sqlsrv_execute($stmt)){
-        //sqlsrv_next_result($stmt);
-        $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-        if($row['IDPEL']==''){
-            sqlsrv_next_result($stmt);
-            $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-        }
-
-        $response['pemeriksaan_lpb'] = $row;
-
-        // sqlsrv_next_result($stmt);
-        // $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-
-        $response['msg'] = 'Data Idpel '.$idpel.' berhasil disimpan';
-        $response['success'] = true;
-
-    }else{
-        $response['success'] = false;
-        $response['msg'] = 'Data Idpel '.$idpel.' gagal disimpan';
-    }
-
+    if(move_uploaded_file($file_tmp, $file_path."/".$file_name))
+      $foto = $file_name;
   }
 }
 
+if(isset($_FILES['image2'])){
+  $file_name = $kk.'.jpg';
+  $file_size =$_FILES['image2']['size'];
+  $file_tmp =$_FILES['image2']['tmp_name'];
+  $file_type=$_FILES['image2']['type'];
+  $file_ext=strtolower(end(explode('.',$_FILES['image2']['name'])));
+
+  $extensions= array("jpeg","jpg","png");
+
+  if(in_array($file_ext,$extensions)=== false){
+     $response['error'][]="extension not allowed, please choose a JPEG or PNG file.";
+  }
+
+  if($file_size > 2097152){
+     $response['error'][]='File size must be excately 2 MB';
+  }
+
+  if(empty($response['error'])==true){
+    if(strlen($unitup)==5)
+      $file_path = "/media/nas/uploads/PRIANGAN/rumah/".$unitap."/".$unitup;
+    else
+      $file_path = "/media/nas/uploads/PRIANGAN/rumah/53XXX";
+    if (!file_exists($file_path)) {
+        mkdir($file_path, 0777, true);
+    }
+    if(move_uploaded_file($file_tmp, $file_path."/".$file_name))
+      $foto_rumah = $file_name;
+  }
+}
+
+sqlsrv_free_stmt($stmt);
+
+$params = array(
+        array($tgl_pemeriksaan, SQLSRV_PARAM_IN),
+        array($unitupi, SQLSRV_PARAM_IN),
+        array($unitap, SQLSRV_PARAM_IN),
+        array($unitup, SQLSRV_PARAM_IN),
+        array($idpel, SQLSRV_PARAM_IN),
+        array($nama, SQLSRV_PARAM_IN),
+        array($tarif, SQLSRV_PARAM_IN),
+        array($daya, SQLSRV_PARAM_IN),
+        array($nik, SQLSRV_PARAM_IN),
+        array($kk, SQLSRV_PARAM_IN),
+        array($nohp, SQLSRV_PARAM_IN),
+        array($email, SQLSRV_PARAM_IN),
+        array($peruntukan, SQLSRV_PARAM_IN),
+        array($sisa_kwh, SQLSRV_PARAM_IN),
+        array($foto, SQLSRV_PARAM_IN),
+        array($foto_rumah, SQLSRV_PARAM_IN),
+        array($latitude, SQLSRV_PARAM_IN),
+        array($longitude, SQLSRV_PARAM_IN),
+        array($akurasi_koordinat, SQLSRV_PARAM_IN),
+        array($user_input, SQLSRV_PARAM_IN),
+        array($tgl_input, SQLSRV_PARAM_IN),
+    );
+
+$sql = "EXEC SP_WS_PEMERIKSAAN_LPB_SIMPAN @TGL_PEMERIKSAAN = ?, @UNITUPI = ?, @UNITAP = ?, @UNITUP = ?, @IDPEL = ?, @NAMA = ?, @TARIF = ?, @DAYA = ?, @NIK = ?, @KK = ?, @NOHP = ?, @EMAIL = ?, @PERUNTUKAN = ?, @SISA_KWH = ?, @FOTO = ?, @FOTO_RUMAH = ?, @LATITUDE = ?, @LONGITUDE = ?, @AKURASI_KOORDINAT = ?, @USER_INPUT = ?, @TGL_INPUT = ? ";
+$stmt = sqlsrv_prepare($conn, $sql, $params);
+
+if(sqlsrv_execute($stmt)){
+    //sqlsrv_next_result($stmt);
+    $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+    if($row['IDPEL']==''){
+        sqlsrv_next_result($stmt);
+        $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+    }
+
+    $response['pemeriksaan_lpb'] = $row;
+
+    // sqlsrv_next_result($stmt);
+    // $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+
+    $response['msg'] = 'Data Idpel '.$idpel.' berhasil disimpan';
+    $response['success'] = true;
+
+}else{
+    $response['success'] = false;
+    $response['msg'] = 'Data Idpel '.$idpel.' gagal disimpan';
+}
 
 sqlsrv_free_stmt($stmt);
 sqlsrv_close($conn);
