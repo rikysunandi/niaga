8a8483bfabcc89d263bf77a9df580ef10ef6d439
rikysunandi <riky.sunandi@gmail.com>
2022-07-13 15:45:53 +0700

    Update sidebar.php

--- bo/main/template/parts/sidebar.php
+++ bo/main/template/parts/sidebar.php
@@ -132,7 +132,7 @@
                     <li><a href="rekomendasi-rpp.php">Rekomendasi RPP</a></li>
                 </ul>
             </li>
-            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="mdi mdi-map"></i> <span class="nav-text">RPP On Site</span></a>
+            <!-- <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="mdi mdi-map"></i> <span class="nav-text">RPP On Site</span></a>
                 <ul aria-expanded="false">
                     <li><a href="pelaksanaan-rpp-onsite.php">Pelaksanaan RPP On Site</a></li>
                     <li><a href="urut-langkah-rpp.php">Pembentukan Urut Langkah RPP</a></li>
@@ -140,7 +140,7 @@
                     <li><a href="rekap-rpp.php">Rekap RPP On Site per Petugas</a></li>
                     <li><a href="daftar-rpp.php">Daftar RPP On Site</a></li>
                 </ul>
-            </li>
+            </li> -->
             <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="mdi mdi-file-document-box"></i> <span class="nav-text">Kompor Induksi</span></a>
                 <ul aria-expanded="false">
                     <li><a href="peta-kompor.php">Peta Pemilihan Target</a></li>
