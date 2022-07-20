bfd2220549aa7fe92554efea84e75d083fe21afd
rikysunandi <riky.sunandi@gmail.com>
2022-07-20 14:16:03 +0700

    Update ftp103.php

--- bo/config/ftp103.php
+++ bo/config/ftp103.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    $ftp_server = "10.2.1.55";
+    $ftp_server = "10.2.1.103";
 	$ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
 	$login = ftp_login($ftp_conn, 'ftpniaga', '123');
 
