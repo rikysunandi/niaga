d83cc5dcabc70b0e9e21355f970eb4bd2ffe6a35
rikysunandi <riky.sunandi@gmail.com>
2022-08-15 15:19:30 +0700

    Create index-appsheet.php

---
+++ bo/main/template/index-appsheet.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>UID Jabar App Sheet Center</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
+    <link href="../css/style.css?time=<?php echo time() ?>" rel="stylesheet">
+    <link href="../css/custom.css?time=<?php echo time() ?>" rel="stylesheet">
+    
+</head>
+
+<body>
+    
+    <?php include 'parts/preloader.php'; ?>
+    
+    <!--**********************************
+        Main wrapper start
+    ***********************************-->
+    <div id="main-wrapper">
+
+
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+            <div class="container-fluid">
+                <div class="row page-titles">
+                    <div class="col p-md-0">
+                        <h4 class="text-center">UID Jabar App Sheet Center</h4>
+                    </div>
+                </div>
+                
+
+                <div class="row">
+                    <div class="col-6 col-sm-6 col-lg-4 col-xl-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <span class="display-4"><i class="icon-target text-danger"></i></span>
+                                    <h2 class="mt-4">PRR - Cek Levering</h2>
+                                    <p>Appsheet untuk melakukan pengecekan Levering PRR</p>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="https://www.appsheet.com/start/fbb0e370-2f96-4e95-a1cf-b64aeead8bba"><i class="ti-mobile"></i>
+                                            <p class="">App</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="javascript:void()"><i class="ti-list"></i>
+                                            <p class="text-muted">Data</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 pt-3"><a class="link text-center d-block text-muted" href="javascript:void()"><i class="ti-bar-chart"></i>
+                                            <p class="text-muted">Report</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-sm-6 col-lg-4 col-xl-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <span class="display-4"><i class="icon-directions text-danger"></i></span>
+                                    <h2 class="mt-4">PRR - Cek Anomali</h2>
+                                    <p>Appsheet untuk melakukan pengecekan Anomali PRR</p>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="https://www.appsheet.com/start/6426fa19-e2ab-4cdc-8525-3e89f8b5b9ed"><i class="ti-mobile"></i>
+                                            <p class="">App</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="javascript:void()"><i class="ti-list"></i>
+                                            <p class="text-muted">Data</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 pt-3"><a class="link text-center d-block text-muted" href="javascript:void()"><i class="ti-bar-chart"></i>
+                                            <p class="text-muted">Report</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-sm-6 col-lg-4 col-xl-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <span class="display-4"><i class="icon-anchor text-warning"></i></span>
+                                    <h2 class="mt-4">TS P2TL - Tahap IV</h2>
+                                    <p>Appsheet untuk melakukan penagihan/pemutusan TS P2TL Tahap IV</p>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="https://www.appsheet.com/start/7692e1e3-36e3-46d3-8ea6-2dcc03713397"><i class="ti-mobile"></i>
+                                            <p class="">App</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="https://docs.google.com/spreadsheets/d/1tuBqLzuAI3hEY0aQF1f_dVqBc6GAFWrS08nyb99cpIU/edit?usp=sharing"><i class="ti-list"></i>
+                                            <p class="">Data</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 pt-3"><a class="link text-center d-block text-muted" href="javascript:void()"><i class="ti-bar-chart"></i>
+                                            <p class="text-muted">Report</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-sm-6 col-lg-4 col-xl-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <span class="display-4"><i class="icon-anchor text-warning"></i></span>
+                                    <h2 class="mt-4">TS P2TL - Belum Realisasi</h2>
+                                    <p>Appsheet untuk melakukan penagihan TS P2TL Belum Realisasi diluar Ex DG</p>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="https://www.appsheet.com/start/a6b9078c-a7ac-43ab-b732-0042370a400a"><i class="ti-mobile"></i>
+                                            <p class="">App</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="https://docs.google.com/spreadsheets/d/1Hv3-72gGxG6fYc5raLGP6n0maxUgmrmZON3iGQNVpnc/edit?usp=sharing"><i class="ti-list"></i>
+                                            <p class="">Data</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 pt-3"><a class="link text-center d-block text-muted" href="javascript:void()"><i class="ti-bar-chart"></i>
+                                            <p class="text-muted">Report</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-sm-6 col-lg-4 col-xl-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <span class="display-4"><i class="icon-note text-success"></i></span>
+                                    <h2 class="mt-4">Kompor Induksi - Survey CKPM</h2>
+                                    <p>Appsheet untuk melakukan Survey CKPM Kompor Induksi</p>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="https://www.appsheet.com/start/ba62a2bf-dfe1-411b-918e-109861956af8"><i class="ti-mobile"></i>
+                                            <p class="">App</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 border-right-1 pt-3"><a class="link text-center d-block text-muted" href="https://docs.google.com/spreadsheets/d/1t8VmSrNa_vn5dTv1jJlqZSK3jyJR4chFf9ENCpPtBGI/edit?usp=sharing"><i class="ti-list"></i>
+                                            <p class="">Data</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-4 pt-3"><a class="link text-center d-block text-muted" href="https://datastudio.google.com/reporting/e6222609-41ea-447f-9270-68b7a71fe377"><i class="ti-bar-chart"></i>
+                                            <p class="">Report</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+
+            </div>
+            <!-- #/ container -->
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+        
+        
+
+    </div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <?php include 'parts/footer.php'; ?>
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <script src="../../assets/plugins/common/common.min.js"></script>
+    <script src="../js/custom.min.js"></script>
+    <script src="../js/settings.js"></script>
+    <script src="../js/gleek.js"></script>
+    
+    <script src="../../assets/plugins/block-ui/jquery.blockUI.js"></script>
+    <script src="../../assets/plugins/chart.js/Chart.bundle.min.js"></script>
+    <script src="../../assets/plugins/wnumb/wNumb.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/emn178/chartjs-plugin-labels/src/chartjs-plugin-labels.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js"></script> -->
+    
+    <!-- <script src="../js/pages/index-mekaga.js"></script> -->
+</body>
+</html>
