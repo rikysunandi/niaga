9db9ee02ade29b47a08d7c14252813ee9a034b37
rikysunandi <riky.sunandi@gmail.com>
2022-10-19 09:54:38 +0700

    auth

--- bo/main/controller/auth.php
+++ bo/main/controller/auth.php
@@ -11,6 +11,22 @@ $password = ($_POST['password']);
 
 $response = array();
 
+
+//LDAP Pusat
+$AD_Server     = "10.1.8.20";
+//$AD_Server     = "10.1.8.22";
+//$AD_Server     = "10.1.8.190";
+//$AD_Server     = '10.2.1.52';
+
+$AD_DomainName = 'DC=pusat,DC=corp,DC=pln,DC=co,DC=id';           // Domain DN
+$AD_UsnPostfix = '@pusat.corp.pln.co.id';                         // username with read access
+//$AD_DomainName = 'DC=jabar,DC=corp,DC=pln,DC=co,DC=id';
+//$AD_UsnPostfix = '@jabar.corp.pln.co.id';
+
+$ldapconn = ldap_connect($AD_Server);
+ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+
 if ($ldapconn) 
 {
     $ldapbind = ldap_bind($ldapconn, $username, $password);
