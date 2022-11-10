b5e8b11cbab21b06f65479016c3ee3f12f94e7db
rikysunandi <riky.sunandi@gmail.com>
2022-11-10 10:13:02 +0700

    update

--- bo/main/template/login.php
+++ bo/main/template/login.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Back Office | Login Page</title>
+    <title>ROC Jabar | Halaman Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="../css/login.css?time=15">
