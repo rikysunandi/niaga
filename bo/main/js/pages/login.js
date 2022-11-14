b646adca73a73fe1973fcb6424d25f159356c37a
rikysunandi <riky.sunandi@gmail.com>
2022-11-14 08:50:47 +0700

    login

--- bo/main/js/pages/login.js
+++ bo/main/js/pages/login.js
@@ -1,6 +1,29 @@
 $(function() {
     "use strict";
 
+    var timeout; //global timout variable that holds reference to timer
+    
+    // $('#login-form').motioncaptcha({
+    //      // Basics:
+    //      action: '#mc-action',        // the ID of the input containing the form action
+    //      divId: '#mc',                // if you use an ID other than '#mc' for the placeholder, pass it in here
+    //      cssClass: '.mc-active',      // this CSS class is applied to the 'mc' div when the plugin is active
+    //      canvasId: '#mc-canvas',      // the ID of the MotionCAPTCHA canvas element
+         
+    //      // An array of shape names that you want MotionCAPTCHA to use:
+    //      shapes: ['triangle', 'x', 'rectangle', 'circle', 'check', 'caret', 'zigzag', 'arrow', 'leftbracket', 'rightbracket', 'v', 'delete', 'star', 'pigtail'],
+         
+    //      // These messages are displayed inside the canvas after a user finishes drawing:
+    //      errorMsg: 'Please try again.',
+    //      successMsg: 'Captcha passed!',
+         
+    //      // This message is displayed if the user's browser doesn't support canvas:
+    //      noCanvasMsg: "Your browser doesn't support <canvas> - try Chrome, FF4, Safari or IE9."
+         
+    //      // This could be any HTML string (eg. '<label>Draw this shit yo:</label>'):
+    //      label: '<p>Please draw the shape in the box to submit the form:</p>'
+    //  });
+
     $('button#btn_login').click(function(){
 
         $('div.login').parent().block({ message: 'Proses Autentikasi...' });
@@ -26,25 +49,26 @@ $(function() {
                     console.log(response);
                     var data = JSON.parse(response);
                     
-                    if(data.success)
-                    {
+                    if(data.success){
                         $('div.container').block({ message: 'Autentikasi berhasil...' });
-                        window.location.href=data.goto_url;
+                        alert(data.goto_url);
+                        //window.location.href=data.goto_url;
                         $('div.container').unblock();
                     }
-                    else
-                    {
-                        // $.notify(data.msg, {
-                        //     type:'warning',
-                        //     placement: {
-                        //         from: 'bottom', 
-                        //         align: 'center'
-                        //     },
-                        //     animate: {
-                        //         enter: 'animated slideInDown',
-                        //         exit: 'animated slideOutUp'
-                        //     }
-                        // });
+                    else{
+                        /*
+                        $.notify(data.msg, {
+                            type:'warning',
+                            placement: {
+                                from: 'bottom', 
+                                align: 'center'
+                            },
+                            animate: {
+                                enter: 'animated slideInDown',
+                                exit: 'animated slideOutUp'
+                            }
+                        });
+                        */
                         alert(data.msg);
                     }
                 }
--- bo/main/template/login.php
+++ bo/main/template/login.php
@@ -11,7 +11,9 @@
     <title>ROC Jabar | Halaman Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="../../assets/plugins/MotionCAPTCHA/jquery.motionCaptcha.0.2.css">
     <link rel="stylesheet" href="../css/login.css?time=15">
+    
 </head>
 
 <body>
@@ -26,7 +28,7 @@
                 <div class="login d-flex align-items-center py-5">
 
                     <div class="container">
-                        <div class="row">
+                        <div id="login-form" class="row">
                             <div class="col-lg-12 col-xl-12 mx-auto">
                                 <h3 class="display-5">ROC (Report Online Commerce)</h3>
                                 <p class="text-muted mb-4"><small>Back Office untuk mengelola hasil pekerjaan pendukung pada Bidang Niaga dan Manajemen Pelanggan</small></p>
@@ -37,10 +39,17 @@
                                     <div class="form-group mb-3">
                                         <input id="xpassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary">
                                     </div>
+<!-- 
+                                     <div id="mc">
+                                         <p>Please draw the shape in the box to submit the form:</p>
+                                         <canvas id="mc-canvas"></canvas>
+                                     </div> -->
+                                    <!-- 
                                     <div class="custom-control custom-checkbox mb-3">
                                         <input id="customCheck1" type="checkbox" checked class="custom-control-input">
                                         <label for="customCheck1" class="custom-control-label">Remember password</label>
                                     </div>
+ -->
                                     <button type="button" id="btn_login" class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
                                     <div class="text-center d-flex justify-content-between mt-4">
                                         <small>*untuk internal PLN, silahkan menggunakan username login email korporat (tanpa pusat)</small>
@@ -58,9 +67,10 @@
     <script src="../../assets/plugins/common/common.min.js"></script>
     <script src="../../assets/plugins/block-ui/jquery.blockUI.js"></script>
     <script src="../../assets/plugins/moment/moment.min.js"></script>
+    <script src="../../assets/plugins/MotionCAPTCHA/jquery.motionCaptcha.0.2.js"></script>
     <script src="../../assets/plugins/bootstrap4-notify/bootstrap-notify.min.js"></script>
     <script src="../js/pages/apps.js?time=6"></script>
-    <script src="../js/pages/login.js?time=2"></script>
+    <script src="../js/pages/login.js?time=3"></script>
 
 </body>
 
