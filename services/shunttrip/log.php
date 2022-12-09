b86e23037e2c3a96e31c70447c89f308ceb4d445
rikysunandi <riky.sunandi@gmail.com>
2022-12-09 11:05:48 +0700

    test

--- services/shunttrip/log.php
+++ services/shunttrip/log.php
@@ -6,44 +6,50 @@ require_once '../../bo/config/database.php';
 ini_set('max_execution_time', 60);
 set_time_limit(60);
 
-$id_device = $_GET['id_device'];
-$nohp = $_GET['nohp'];
-$tegangan = $_GET['tegangan'];
-$arus = $_GET['arus'];
-$power_factor = $_GET['power_factor'];
-$power = $_GET['power'];
-$energi = $_GET['energi'];
-$frekuensi = $_GET['frekuensi'];
-$indikator = $_GET['indikator'];
-$tgl_kirim_device = $_GET['tgl_kirim_device'];
+$id_device = $_POST['id_device'];
+$nohp = $_POST['nohp'];
+$tegangan = $_POST['tegangan'];
+$arus = $_POST['arus'];
+$power_factor = $_POST['power_factor'];
+$power = $_POST['power'];
+$energi = $_POST['energi'];
+$frekuensi = $_POST['frekuensi'];
+$indikator = $_POST['indikator'];
+$tgl_kirim_device = $_POST['tgl_kirim_device'];
+$key = $_POST['key'];
 $user = 'SYSTEM';
 
 $response = array();
 
+if($key=='338ef7d4-0368-5aa0-9c0b-4aae493f7eb7'){
 
-$params = array(
-        array($id_device, SQLSRV_PARAM_IN),
-        array($nohp, SQLSRV_PARAM_IN),
-        array($tegangan, SQLSRV_PARAM_IN),
-        array($arus, SQLSRV_PARAM_IN),
-        array($power_factor, SQLSRV_PARAM_IN),
-        array($power, SQLSRV_PARAM_IN),
-        array($energi, SQLSRV_PARAM_IN),
-        array($frekuensi, SQLSRV_PARAM_IN),
-        array($indikator, SQLSRV_PARAM_IN),
-        array($tgl_kirim_device, SQLSRV_PARAM_IN),
-    );
+    $params = array(
+            array($id_device, SQLSRV_PARAM_IN),
+            array($nohp, SQLSRV_PARAM_IN),
+            array($tegangan, SQLSRV_PARAM_IN),
+            array($arus, SQLSRV_PARAM_IN),
+            array($power_factor, SQLSRV_PARAM_IN),
+            array($power, SQLSRV_PARAM_IN),
+            array($energi, SQLSRV_PARAM_IN),
+            array($frekuensi, SQLSRV_PARAM_IN),
+            array($indikator, SQLSRV_PARAM_IN),
+            array($tgl_kirim_device, SQLSRV_PARAM_IN),
+        );
 
-$sql = "EXEC SP_WS_SHUNTRIP_LOG_SIMPAN @id_device = ?, @nohp = ?, @tegangan = ?, @arus = ?, @power_factor = ?, @vpower = ?, @energi = ?, @frekuensi = ?, @indikator = ?, @tgl_kirim_device = ? ";
-$stmt = sqlsrv_prepare($conn, $sql, $params);
+    $sql = "EXEC SP_WS_SHUNTRIP_LOG_SIMPAN @id_device = ?, @nohp = ?, @tegangan = ?, @arus = ?, @power_factor = ?, @vpower = ?, @energi = ?, @frekuensi = ?, @indikator = ?, @tgl_kirim_device = ? ";
+    $stmt = sqlsrv_prepare($conn, $sql, $params);
 
-if(sqlsrv_execute($stmt)){
-    $response['success'] = true;
-    $msg = '#1#Data berhasil disimpan';
+    if(sqlsrv_execute($stmt)){
+        $response['success'] = true;
+        $msg = '#1#Data berhasil disimpan';
 
+    }else{
+        $response['success'] = false;
+        $msg = '#0#Data gagal disimpan';
+    }
 }else{
     $response['success'] = false;
-    $msg = '#0#Data gagal disimpan';
+    $msg = '#0#Key tidak valid!';
 }
 
 
