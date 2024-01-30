51285da76875d0ff1975525d46916528d964b0c8
rikysunandi <riky.sunandi@gmail.com>
2024-01-30 12:59:57 +0700

    Update spklu_unavailable.php

--- bo/main/template/spklu_unavailable.php
+++ bo/main/template/spklu_unavailable.php
@@ -191,7 +191,7 @@ if($data->message=='success'){
 			$txt_group .= '------------------------- '.$break.$break;
 
 			while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-				$txt_group .= generate_notif_unavailable($data, $row);
+				$txt_group .= ($i+1).generate_notif_unavailable($data, $row);
 
 				$i++;
 			}
