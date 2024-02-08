96e1dd29e5909665fdb8b563e73010ad86e73c25
rikysunandi <riky.sunandi@gmail.com>
2024-02-08 21:49:09 +0700

    Update spklu_unavailable.php

--- bo/main/template/spklu_unavailable.php
+++ bo/main/template/spklu_unavailable.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="refresh" content="150" />
+	<meta http-equiv="refresh" content="90" />
 	<title>Update SPKLU Unavailable</title>
 </head>
 <body>
@@ -119,7 +119,7 @@ if($data->message=='success'){
 			
             $txt = generate_notif_ok($data, $row);
             $response = send_wa_message($txt, $row['TL_TE_WA']);
-            //sleep(rand(1,3));
+            sleep(rand(1,3));
             $response = send_wa_message($txt, $row['MULP_WA']);
             sleep(rand(1,3));
             $response = send_wa_message($txt, $row['CC_WA1']);
@@ -157,7 +157,7 @@ if($data->message=='success'){
 			$txt .= 'Ini adalah pesan satu arah, mohon untuk tidak membalas. ';
             
             $response = send_wa_message($txt, $row['TL_TE_WA']);
-            //sleep(rand(1,3));
+            sleep(rand(1,3));
             $response = send_wa_message($txt, $row['MULP_WA']);
             sleep(rand(1,3));
             $response = send_wa_message($txt, $row['CC_WA1']);
