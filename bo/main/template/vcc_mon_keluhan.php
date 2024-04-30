004417da35e7df4bdf5acd6c6ca24901fecad649
rikysunandi <riky.sunandi@gmail.com>
2024-04-30 11:02:34 +0700

    Update vcc_mon_keluhan.php

--- bo/main/template/vcc_mon_keluhan.php
+++ bo/main/template/vcc_mon_keluhan.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="refresh" content="120" />
+	<meta http-equiv="refresh" content="90" />
 	<title>VCC Mon Keluhan</title>
 </head>
 <body>
