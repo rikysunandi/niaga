2d3b130b4d7758a2194652524af3f0b68de57541
rikysunandi <riky.sunandi@gmail.com>
2024-07-03 17:40:37 +0700

    Update tmp_detail_gangguan.php

--- bo/main/template/tmp_detail_gangguan.php
+++ bo/main/template/tmp_detail_gangguan.php
@@ -317,6 +317,6 @@ sqlsrv_free_stmt($stmtunit);
 sqlsrv_close($conn);
 
 function clean($str){
-	return preg_match('/^[a-z0-9 .\-]+$/i', $str)
+	return preg_match('/^[a-z0-9 .\-]+$/i', $str);
 }
 ?>
